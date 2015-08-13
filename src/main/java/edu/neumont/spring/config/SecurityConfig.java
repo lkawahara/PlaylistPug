4c275e50ac20123d4b6cab9f5d1c26c9313e2cc2
Bastien Auxer <auxerbastien@gmail.com>
2015-08-13 17:59:14 -0600

    heroku project coming up -BA

--- src/main/java/edu/neumont/spring/config/SecurityConfig.java
+++ src/main/java/edu/neumont/spring/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
