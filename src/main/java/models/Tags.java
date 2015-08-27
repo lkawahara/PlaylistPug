6cdcc207cf9c9395eabd61760eba6ebd691291eb
Layne Kawahara <laynekawahara@gmail.com>
2015-08-26 18:13:06 -0600

    Changes to Tags

--- src/main/java/models/Tags.java
+++ src/main/java/models/Tags.java
@@ -2,5 +2,36 @@ package models;
 
 public enum Tags 
 {
+	//provide display name for display names of more than one word or with special characters
+		//all display names will be lower case
+	Asian,
+	AvantGarde("avant-garde"),
+	Blues,
+	Classical,
+	Country,
+	EasyListening("easy listening"),
+	Electronic,
+	HipHop("hip-hop"), 
+	Instrumental,
+	Jazz,
+	Latin,
+	Pop,
+	Punk,
+	Rap,
+	RB("r & b"),
+	Reggae,
+	Rock,
+	Ska,
+	Soul;
 	
+	private String displayName;
+	Tags(){
+		this.displayName = this.toString().toLowerCase();
+	}
+	Tags(String displayName){
+		this.displayName = displayName.toLowerCase();
+	}
+	public String GetDisplayName(){
+		return this.displayName;
+	}
 }
