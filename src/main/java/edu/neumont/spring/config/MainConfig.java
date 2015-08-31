b6fd78fd98a022e7f4e8716c153967db75523dd4
Bastien Auxer <auxerbastien@gmail.com>
2015-08-30 18:14:47 -0600

    Take three

--- src/main/java/edu/neumont/spring/config/MainConfig.java
+++ src/main/java/edu/neumont/spring/config/MainConfig.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MainConfig 
 {
+	/*
 	private static final String DATABASE_URL = "postgres://maphiawdhrurgw:PL4ApC1CK2qpN0XxBCfNJwehBU@ec2-54-163-228-0.compute-1.amazonaws.com:5432/d9tiku6olid4s7";
 	
     @Bean(name="dataSource")
@@ -34,13 +35,13 @@ public class MainConfig
         String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "";
         
-        /* Spring/Java && JDBC*/
+        // Spring/Java && JDBC
         dbUrl = "jdbc:postgresql://" +
 		        dbUri.getHost() + ':' +
 		        dbUri.getPort() +
 		        dbUri.getPath() +
 		        "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-        /**/
+
         
         @SuppressWarnings("unused")
 		Connection connection = DriverManager.getConnection(dbUrl, username, password);
@@ -52,4 +53,5 @@ public class MainConfig
         
         return basicDataSource;
     }
+*/
 }
