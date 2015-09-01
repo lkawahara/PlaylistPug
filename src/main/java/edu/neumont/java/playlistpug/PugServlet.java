35fcf7cac2458d327d8b122fb5cd5670461bb44a
Bastien Auxer <auxerbastien@gmail.com>
2015-08-31 18:38:08 -0600

    Fixed DummyDAl to follow the right the right enum - BA

--- src/main/java/edu/neumont/java/playlistpug/PugServlet.java
+++ src/main/java/edu/neumont/java/playlistpug/PugServlet.java
@@ -33,8 +33,6 @@ public class PugServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		IDALpug dallPug = (IDALpug) DependencyInjector.getInstance().get("IDALpug");
-		
 		String destination = request.getPathInfo();
 		String[] path = null;
 		String error = "You done goofed.";
@@ -50,7 +48,7 @@ public class PugServlet extends HttpServlet {
 				destination = "/WEB-INF/upload.jsp";
 			}else if(path[0].equals("song")){
 				//set attributes name as the name of the song and
-				//song as whatever is needed to play the song
+				//song as whatever is needed to play the songs
 				request.setAttribute("name", "song name");
 				request.setAttribute("song", "song data");
 				request.setAttribute("sid", 1);
